b0ca39c0db5e8a3962610549a8659a365db76412
abzokhattab <abdelrahman@energenious>
2023-03-12 02:29:21 +0200

    fixing vercel

--- server/dist/index.js
+++ server/dist/index.js
@@ -22,4 +22,4 @@ app.use("/api", routes_1.default);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 exports.default = app;
-//# sourceMappingURL=app.js.map
+//# sourceMappingURL=index.js.map
